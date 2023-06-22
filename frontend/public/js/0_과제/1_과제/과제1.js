c51ccbf034612f076f958668e21a49a9171e55c8
506 <506@DESKTOP-LDJSJ8L>
2023-06-22 21:09:45 +0900

    0622 21:09

--- frontend/public/js/0_과제/1_과제/과제1.js
+++ frontend/public/js/0_과제/1_과제/과제1.js
@@ -1,3 +1,30 @@
 /**
  * 
  */
+
+ // 문제 11 : 정수 3개를 입력받아 오름차순 출력 
+let int1 = Number( prompt('정수1 : ') ) ;	
+let int2 = Number( prompt('정수2 : ') ) ;	
+let int3 = Number( prompt('정수3 : ') ) ;	
+
+if( int1 > int2 ){
+	let temp = int1; 
+	int1 = int2;	 
+	int2 = temp;}
+if( int1 > int3 ){let temp = int1; int1 = int3; int3 = temp;}
+if( int2 > int3 ){let temp = int2; int2 = int3; int3 = temp;}
+console.log( int1 +'  '+int2 + '  ' + int3 )
+
+/* 문제12 : [ 가위바위보 게임 ] 
+
+
+
+
+
+
+
+
+
+
+
+*/
--- frontend/public/js/0_과제/2_과제/과제2.js
+++ frontend/public/js/0_과제/2_과제/과제2.js
@@ -1,5 +1,15 @@
-/**
- * 
- */
+// 배열 선언 
+let 회원가입=[];
+let 로그인=[];
 
- 
+// 이벤트 함수 만들기 
+function 회원가입(){
+let contentInput = document.querySelector('.id_name .pw_name');
+
+
+let value = contentInput.value
+
+
+회원가입.push( value )
+}
+let contentTable = document.querySelector('.contentTable'); console.log( contentTable )
