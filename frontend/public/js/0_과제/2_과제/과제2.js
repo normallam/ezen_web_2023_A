bd58e102a1aa4b6a131bfc7a64538a5389f0585e
504 <504@DESKTOP-T7B8DGK>
2023-06-23 11:53:02 +0900

    0623 open

--- frontend/public/js/0_과제/2_과제/과제2.js
+++ frontend/public/js/0_과제/2_과제/과제2.js
@@ -1,15 +1,37 @@
 // 배열 선언 
-let 회원가입=[];
-let 로그인=[];
+let 회원가입배열=[];
+let 로그인배열=[];
 
 // 이벤트 함수 만들기 
 function 회원가입(){
-let contentInput = document.querySelector('.id_name .pw_name');
+let 아이디입력창 = document.querySelector('.id_sign_up');
+// "id_sign_upInput"라는 변수를 선언했어 // 문서에 셀렉터이름이  
+let 비밀번호입력창 = document.querySelector('.pw_sign_up');
+
+
+
+alert('회원가입되셨습니다.')
+
+
+
 
 
 let value = contentInput.value
+회원가입배열.indexOf(name) != -1 ? alert('이미 존재하는 학생명입니다[추가불가]') : 회원가입배열.push( name )}
 
-
-회원가입.push( value )
-}
 let contentTable = document.querySelector('.contentTable'); console.log( contentTable )
+
+
+function 로그인(){
+	
+let 로그인아이디 = document.querySelector('.id_sign_in');
+let 로그인비밀번호 = document.querySelector('.pw_sign_in');	
+	
+alert('[로그인 성공]')
+alert('[로그인 실패]')	
+	
+let value = contentInput.value;	console.log( value );		
+
+로그인.push( value )}
+	
+	
