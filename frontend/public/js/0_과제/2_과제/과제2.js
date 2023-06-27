70fc56a47d7097dec0ef049d017b5d7deb896d5a
l <l@DESKTOP-DK8MU3B>
2023-06-28 01:49:40 +0900

    01:50 과제2

--- frontend/public/js/0_과제/2_과제/과제2.js
+++ frontend/public/js/0_과제/2_과제/과제2.js
@@ -2,36 +2,36 @@
 let 회원가입배열=[];
 let 로그인배열=[];
 
+
+console.log('js 열림');
+
 // 이벤트 함수 만들기 
-function 회원가입(){
-let 아이디입력창 = document.querySelector('.id_sign_up');
+function 회원가입(){console.log('회원가입함수 클릭');
+let 아이디입력창 = document.querySelector('.id_sign_up'); console.log(아이디입력창)
 // "id_sign_upInput"라는 변수를 선언했어 // 문서에 셀렉터이름이  
-let 비밀번호입력창 = document.querySelector('.pw_sign_up');
-
-
-
+let 비밀번호입력창 = document.querySelector('.pw_sign_up'); console.log(비밀번호입력창)
 alert('회원가입되셨습니다.')
 
 
+let id_sign_up = 아이디입력창.value; console.log(id_sign_up);
+let pw_sign_up = 비밀번호입력창.value; console.log(pw_sign_up);
 
+}
 
-
-let value = contentInput.value
-회원가입배열.indexOf(name) != -1 ? alert('이미 존재하는 학생명입니다[추가불가]') : 회원가입배열.push( name )}
-
-let contentTable = document.querySelector('.contentTable'); console.log( contentTable )
 
 
 function 로그인(){
 	
-let 로그인아이디 = document.querySelector('.id_sign_in');
-let 로그인비밀번호 = document.querySelector('.pw_sign_in');	
+let 로그인아이디 = document.querySelector('.id_sign_in'); console.log(로그인아이디)
+let 로그인비밀번호 = document.querySelector('.pw_sign_in');	console.log(로그인비밀번호)
 	
-alert('[로그인 성공]')
-alert('[로그인 실패]')	
+let id_sign_in = 로그인아이디.value; console.log(id_sign_in);
+let pw_sign_in = 로그인비밀번호.value; console.log(pw_sign_in);	
 	
-let value = contentInput.value;	console.log( value );		
+	if(id_sign_in==로그인배열){"[로그인 실패]"}
+	else if("[로그인 성공]")	
+
+	
 
 로그인.push( value )}
-	
 	
