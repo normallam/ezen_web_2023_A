477f9aa039f218cb9971ad59672e188ab3552ae0
506 <506@DESKTOP-LDJSJ8L>
2023-06-27 21:50:24 +0900

    21:50

--- frontend/public/js/0_과제/1_과제/과제1.js
+++ frontend/public/js/0_과제/1_과제/과제1.js
@@ -15,16 +15,15 @@ if( int1 > int3 ){let temp = int1; int1 = int3; int3 = temp;}
 if( int2 > int3 ){let temp = int2; int2 = int3; int3 = temp;}
 console.log( int1 +'  '+int2 + '  ' + int3 )
 
-/* 문제12 : [ 가위바위보 게임 ] 
+// 문제12 : [ 가위바위보 게임 ] 
+
+let player1 = prompt('가위(0)바위(1)보(2) 중에 하나 입력:')
+
+let player2 = prompt('가위(0)바위(1)보(2) 중에 하나 입력:')
 
 
-
-
-
-
-
-
-
-
-
-*/
+ if(player1==0 && player2==2 || player1==1 && player2==0||player1==2 && player2==1)
+  {console.log('플레이어1 승리')}
+ else if(player2==player1)
+ {console.log('무승부')}
+ else{console.log('플레이어2 승리')}
