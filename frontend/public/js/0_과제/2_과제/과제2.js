da7fb7a7417c82b96d162a38ec388dde63d95016
504 <504@DESKTOP-FDBPKQC>
2023-06-28 18:41:38 +0900

    0628 18:40

--- frontend/public/js/0_과제/2_과제/과제2.js
+++ frontend/public/js/0_과제/2_과제/과제2.js
@@ -1,24 +1,26 @@
 // 배열 선언 
-let 회원가입배열=[];
-let 로그인배열=[];
-
+let 아이디배열=[];
+let 비밀번호배열=[];
 
 console.log('js 열림');
 
 // 이벤트 함수 만들기 
 function 회원가입(){console.log('회원가입함수 클릭');
 let 아이디입력창 = document.querySelector('.id_sign_up'); console.log(아이디입력창)
-// "id_sign_upInput"라는 변수를 선언했어 // 문서에 셀렉터이름이  
 let 비밀번호입력창 = document.querySelector('.pw_sign_up'); console.log(비밀번호입력창)
-alert('회원가입되셨습니다.')
 
 
 let id_sign_up = 아이디입력창.value; console.log(id_sign_up);
 let pw_sign_up = 비밀번호입력창.value; console.log(pw_sign_up);
 
+if(id_sign_up.length<8||pw_sign_up.length<8){alert('[회원가입 실패]')}{'[회원가입 성공]'}
+if( mid == '' || mpwd == '' ) { alert('[회원가입실패] : 회원정보 모두 입력해주세요.');
+
 }
+ 아이디배열.push( id_sign_up );
+ 비밀번호배열.push( pw_sign_up  );
 
-
+ alert('회원가입되셨습니다.')
 
 function 로그인(){
 	
@@ -27,11 +29,8 @@ let 로그인비밀번호 = document.querySelector('.pw_sign_in');	console.log(�
 	
 let id_sign_in = 로그인아이디.value; console.log(id_sign_in);
 let pw_sign_in = 로그인비밀번호.value; console.log(pw_sign_in);	
-	
-	if(id_sign_in==로그인배열){"[로그인 실패]"}
-	else if("[로그인 성공]")	
-
-	
-
-로그인.push( value )}
-	
+		
+		for( let i = 0  ; i < 아이디배열.length ; i++ ){
+		if( 아이디[i] == 입력아이디 && 비밀번호배열[i] == 입력비밀번호 ) { '[로그인 성공]' }
+{'[로그인 실패]'}	
+}
--- frontend/public/js/0_과제/3_과제/과제3.js
+++ frontend/public/js/0_과제/3_과제/과제3.js
@@ -157,7 +157,7 @@ function 등록(){ 	console.log('등록함수 클릭');
 
 function 출력(){ console.log('가계부목록 출력합니다.');
 	// 1. 어디에 출력할껀지??? table 가져오기 
-	let outputTalbe = document.querySelector('.outputTalbe')
+	let outputTable = document.querySelector('.outputTable')
 	// 2. 무엇을???? 
 	let html = `<tr> <th> 날짜 </th> <th> 항목 </th> <th> 가격 </th> <th> 비고 </th> </tr>`
 				
@@ -186,7 +186,7 @@ function 출력(){ console.log('가계부목록 출력합니다.');
 				<td colspan="2" > ${ 총합계.toLocaleString() }원</td>
 			<tr>`
 	// 3. table 에 HTML  넣어주기  <table> inner </table>
-	outputTalbe.innerHTML = html ;
+	outputTable.innerHTML = html ;
 } // f end 
 
 // 3. 
