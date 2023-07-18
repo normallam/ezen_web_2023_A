04f50da2dac9cac4d65caaa3900fe40b7495e3f9
504 <504@DESKTOP-FDBPKQC>
2023-07-18 17:43:27 +0900

    0718 17:43 class 전날꺼 다시 복구

---
+++ backend/src/java1/day04/Ex2_확인문제7.java
@@ -0,0 +1,35 @@
+package java1.day04;
+
+public class Ex2_확인문제7 {//class s
+
+	public static void main(String[] args) {//main s
+		
+		System.out.println("\n----------------------------");
+		System.out.println("1.예금|2.출금|3.잔고|4.종료");
+		System.out.println("----------------------------");
+		System.out.println("선택> 1");
+		System.out.println("예금액> 10000");
+		
+		System.out.println("\n----------------------------");
+		System.out.println("1.예금|2.출금|3.잔고|4.종료");
+		System.out.println("----------------------------");
+		System.out.println("선택>2");
+		System.out.println("출금액> 2000");
+
+		System.out.println("\n----------------------------");
+		System.out.println("1.예금|2.출금|3.잔고|4.종료");
+		System.out.println("----------------------------");
+		System.out.println("선택>3");
+		System.out.println(" 잔고> 8000");
+
+		System.out.println("\n----------------------------");
+		System.out.println("1.예금|2.출금|3.잔고|4.종료");
+		System.out.println("----------------------------");
+		System.out.println("선택>4");
+		System.out.println("예금액> 10000");
+
+		
+	}//main e
+	
+	
+} //class e
