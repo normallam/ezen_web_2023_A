1ceb669b5d2804cc842a90d1175cca838cae156c
504 <504@DESKTOP-OM2NF7O>
2023-09-09 16:21:45 +0900

    ㅇㅇ

--- jspweb/src/main/java/controller/hrm/HrmController.java
+++ jspweb/src/main/java/controller/hrm/HrmController.java
@@ -1,11 +1,18 @@
 package controller.hrm;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import model.dao.HrmDao;
+import model.dto.HrmDto;
 
 
 @WebServlet("/HrmController")
@@ -20,7 +27,8 @@ public class HrmController extends HttpServlet {
 
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
+		ArrayList<HrmDto> result = HrmDao.getInstance().view();
+		ObjectMapper objectMapper = new 
 	}
 
 	
